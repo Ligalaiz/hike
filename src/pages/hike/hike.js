a7b076245310fe1c8cb7b335b1511ce2eb135e9a
ligalaiz <slavakuz88@gmail.com>
2022-11-26 20:14:48 +0300

    refactor: import files

--- src/pages/hike/hike.js
+++ src/pages/hike/hike.js
@@ -1,1 +1,6 @@
+import '@src/utils/showHeader.utils';
+import '@src/utils/smoothScroll.utils';
+import '@src/utils/dropdown.utils';
+import '@src/utils/timer.utils';
+
 import '@src/assets/styles/global/index.scss';
